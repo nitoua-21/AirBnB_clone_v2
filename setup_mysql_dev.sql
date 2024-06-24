b2ad9bc009c5549018a11e9691917c08f1d8bb17
Raheem Oluwatobiloba <raim.tobi@gmail.com>
2024-06-24 22:38:15 +0100

    feat: setup mysql DB

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- Create a mySQL DB
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant priviledges as required
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply new changes
+FLUSH PRIVILEGES
