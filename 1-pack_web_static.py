f17b620949b3f4a97555b851d94f008c094ceec8
Normich ITOUA <nitoua.dev@gmail.com>
2024-07-24 02:11:21 -0700

    Add utomated scripts

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Fabric script generates .tgz archive of all in web_static/ using func 'do_pack'
+Usage: fab -f 1-pack_web_static.py do_pack
+"""
+from fabric.api import local
+from strftime import time
+
+
+
+def do_pack():
+    """Generate .tgz archive of web_static/ folder"""
+    time_format = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        filename = "versions/web_static_{}.tgz".format(time_format)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
+    except:
+        return None
+
